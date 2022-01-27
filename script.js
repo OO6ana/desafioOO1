5ae37c786b46ffe9d7124761b59bed3a5c0b8ae0
OO6ana <analuizacoelhodemoura343@gmail.com>
2022-01-27 11:06:32 -0300

    Desafio finalizado

---
+++ script.js
@@ -0,0 +1,31 @@
+// Crie uma variável que receba um nome;
+
+let nome = "Ana Luiza"
+console.log(nome)
+
+// Crie uma variável que receba um número;
+
+let idade = 15
+console.log(idade)
+
+// Crie uma variável com o ano atual e subtraia pelo número que você guardou;
+
+let ano = 2022 - 15
+console.log(ano)
+
+// Agora mostre no console o resultado da soma das duas variáveis guardadas anteriormente;
+
+let anoSoma = 2022 + 15
+console.log(anoSoma)
+
+//Mostre no console a mensagem "Estou mexendo no console.log";
+
+console.log(` ${nome} está mexendo no console.log`)
+
+// Crie uma variável chamada “quartaFeira” e atribua à ela o valor booleano que representa verdadeiro ou falso e mostre a saída no console;
+
+let quartaFeira = false
+console.log(quartaFeira)
+
+// Exiba no console o tipo de dado da váriavel quartaFeira;
+console.log("Quarta-Feira é um dado boolean")
